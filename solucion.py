4c0513eed4bc2308a16c54551d3151d8e6c3f09d
danielescolar43-maker <danielescolar43@gmail.com>
2025-11-28 22:59:25 -0700

    Update solucion.py

--- solucion.py
+++ solucion.py
@@ -2,6 +2,33 @@
 
 def reloj_arena(m: int, s: str) -> str:
     # TODO: validar altura mayor que 0 e imprimir "Error: La altura debe ser un entero positivo" y salir
-    
+    if m <= 0:     # para que la altura sea correcta
+        return "Error: La altura debe ser un entero positivo"
     # TODO: implementar la lógica para generar el reloj de arena en ASCII
-    pass
+    lineas = []        # Para la parte superior, es un triángulo invertido y así queda como en la referencia
+    espacios = 0
+    cantidad = m
+
+    while cantidad > 0:
+        lineas.append(" " * espacios + s * (2 * cantidad - 1))
+        espacios += 1
+        cantidad -= 1
+
+    espacios -= 2   # Parte de abajo de la figura, el triengulo normalito
+    cantidad = 2
+
+    while cantidad <= m:
+        lineas.append(" " * espacios + s * (2 * cantidad - 1))
+        espacios -= 1
+        cantidad += 1
+
+    
+    return "\n".join(lineas)     # para regresar a toda la figura como un solo string
+
+    # En este archivo debes implementar la función
+
+
+   
+    
+
+    
